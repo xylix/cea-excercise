b5763d5cfd3feadfc167e847cda96ccbf4020333
Kerkko Pelttari <kerk.pelt@gmail.com>
2021-03-20 11:04:03 +0200

    Working impl

--- editor.py
+++ editor.py
@@ -1,15 +1,55 @@
-operations = input()
+from typing import Tuple, Literal, Union
 
-for i in range(0, int(operations)):
+Op = Literal["1", "2", "3", "4"]
+
+
+def append(S: str, W: str):
+    S = S + W
+    last_op = ("1", W)
+    return S, last_op
+
+
+def delete(S: str, k: int):
+    diff = S[0:k]
+    # print(f"diff: {diff}")
+    S = S[:-k]
+    last_op = ("2", diff)
+    return S, last_op
+
+
+Operation = Union[Tuple[Literal["1", "2"], str]]
+
+
+def undo(S: str, last_op: Operation):
+    last_op_type, last_param = last_op
+    if last_op_type == "1":
+        S, _ = delete(S, len(last_param))
+    elif last_op_type == "2":
+        S, _ = append(S, last_param)
+    return S
+
+
+Q = int(input())
+
+S: str = ""
+op_list = []
+for i in range(0, Q):
     operation = input()
-    op_type, param = operation.split(' ')
-    if op_type == 1:
-        pass
-    elif op_type == 2:
-        pass
-    elif op_type == 3:
-        pass
-    elif op_type == 4:
-        pass
+    # print(f"Op list: {op_list}")
+    if operation == "4":
+        S = undo(S, op_list.pop())
     else:
-        raise NotImplementedError()
+        t, param = operation.split(" ")
+        if t == "1":
+            S, last_op = append(S, param)
+            op_list.append(last_op)
+        elif t == "2":
+            k = int(param)
+            S, last_op = delete(S, k)
+            op_list.append(last_op)
+        elif t == "3":
+            k = int(param) - 1
+            # print(f"k: {k}", f"S: {S}")
+            print(S[k])
+        else:
+            raise NotImplementedError()
