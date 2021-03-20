1b2e9caf28c172ef92d90365f3b54c899d9c7234
Kerkko Pelttari <kerk.pelt@gmail.com>
2021-03-20 10:27:05 +0200

    Implement stub editor

---
+++ editor.py
@@ -0,0 +1,15 @@
+operations = input()
+
+for i in range(0, int(operations)):
+    operation = input()
+    op_type, param = operation.split(' ')
+    if op_type == 1:
+        pass
+    elif op_type == 2:
+        pass
+    elif op_type == 3:
+        pass
+    elif op_type == 4:
+        pass
+    else:
+        raise NotImplementedError()
